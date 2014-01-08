85a57917a8c455931be7f0b5f91efcb775603ddc
zhonghua <huajuanatmac@gmail.com>
2014-01-08 10:00:43 +0000

    Added code for headers

--- weatherbasic.R
+++ weatherbasic.R
@@ -1,3 +1,13 @@
 w = read.table('1999_01_19', header=FALSE, comment.char ="#", sep = "\t")
 w
 summary(w)
+
+hd <- readLines(f)
+hd <- hd[grep("#", hd)]
+hd <- sub("#", "", hd)
+hd <- hd[7:8]
+hd <- gsub(" ", "", hd)
+hd <- strsplit(hd, "\t")
+hd <- paste0(hd[[1]], " [", hd[[2]], "]")
+hd <- sub(" \\[\\]", "", hd)
+names(w) <- hd
