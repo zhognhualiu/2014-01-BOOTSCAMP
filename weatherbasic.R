9f7ed39c9267ff8466417faf2c100a1d1791ef6a
zhonghua <huajuanatmac@gmail.com>
2014-01-08 10:41:54 +0000

    Plotted time with wind speed

--- weatherbasic.R
+++ weatherbasic.R
@@ -11,3 +11,9 @@ hd <- strsplit(hd, "\t")
 hd <- paste0(hd[[1]], " [", hd[[2]], "]")
 hd <- sub(" \\[\\]", "", hd)
 names(w) <- hd
+par(mfrow = c(2, 2))
+plot(w$Time, w[, "Temp [degC]"], type = "b", xlab = "Time", ylab = 
+"Temp")
+
+
+
